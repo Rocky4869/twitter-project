99bfac34d16afc879608e81366a98f695c3af6f5
BillyNgEng <symmetrictensor@gmail.com>
2023-03-31 16:03:14 +0800

    Tweet

--- twitter-project/src/TweetBox.js
+++ twitter-project/src/TweetBox.js
@@ -1,23 +1,59 @@
-import React from 'react';
+import React, { useState } from "react";
 import "./TweetBox.css";
-import { Avatar, Button } from '@material-ui/core';
+import { Avatar, Button } from "@material-ui/core";
+import db from "./firebase";
 
 function TweetBox() {
-    return(
-        <div className='tweetBox'>
-            <form>
-                <div className='tweetBox_input'>
-                <Avatar style={{ height: '140px', width: '140px' }} src="https://dep.com.vn/wp-content/uploads/2022/11/phong-cach-thoi-trang-cha-eun-woo-1.jpg"></Avatar>
-                {/*
-                <Avatar src="https://kajabi-storefronts-production.global.ssl.fastly.net/kajabi-storefronts-production/themes/284832/settings_images/rLlCifhXRJiT0RoN2FjK_Logo_roundbackground_black.png"></Avatar>
-                */}
-                <input placeholder="What's happening?"type="text"/>
-                </div>
-                <input className='tweetBox_imageInput' placeholder="Optional: Enter image URL" type="text"/>
-                <Button className='tweetBox_tweetButton'>Tweet</Button>
-            </form>
+  const [tweetMessage, setTweetMessage] = useState("");
+  const [tweetImage, setTweetImage] = useState("");
+
+  const sendTweet = (e) => {
+    e.preventDefault();
+
+    db.collection("posts").add({
+      displayName: "Cha Eun Woo",
+      username: "eunwo.o_c",
+      verified: true,
+      text: tweetMessage,
+      image: tweetImage,
+      avatar:
+        "https://dep.com.vn/wp-content/uploads/2022/11/phong-cach-thoi-trang-cha-eun-woo-1.jpg",
+    });
+
+    setTweetMessage("");
+    setTweetImage("");
+  };
+
+  return (
+    <div className="tweetBox">
+      <form>
+        <div className="tweetBox_input">
+          <Avatar style={{ height: '140px', width: '140px' }} src="https://dep.com.vn/wp-content/uploads/2022/11/phong-cach-thoi-trang-cha-eun-woo-1.jpg" />
+          <input
+            onChange={(e) => setTweetMessage(e.target.value)}
+            value={tweetMessage}
+            placeholder="What's happening?"
+            type="text"
+          />
         </div>
-    )
+        <input
+          value={tweetImage}
+          onChange={(e) => setTweetImage(e.target.value)}
+          className="tweetBox_imageInput"
+          placeholder="Optional: Enter image URL"
+          type="text"
+        />
+
+        <Button
+          onClick={sendTweet}
+          type="submit"
+          className="tweetBox_tweetButton"
+        >
+          Tweet
+        </Button>
+      </form>
+    </div>
+  );
 }
 
-export default TweetBox
+export default TweetBox;
