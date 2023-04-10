3f134273325c562421633d406db4257c5e063ff0
Rocky4869 <rockytam4869@gmail.com>
2023-04-10 22:42:44 +0800

    basic ui improve + tweet input textfield amend

--- frontend/src/TweetBox.js
+++ frontend/src/TweetBox.js
@@ -1,11 +1,16 @@
 import React, { useState } from "react";
 import "./css/TweetBox.css";
-import { Avatar, Button } from "@material-ui/core";
+import { Avatar, Button, TextField } from "@material-ui/core";
 import db from "./firebase";
+import { IconButton } from "@mui/material";
+import { InsertEmoticon, Image } from "@material-ui/icons";
+import Picker from "emoji-picker-react";
 
 function TweetBox() {
   const [tweetMessage, setTweetMessage] = useState("");
   const [tweetImage, setTweetImage] = useState("");
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
 
   const sendTweet = (e) => {
     e.preventDefault();
@@ -24,26 +29,127 @@ function TweetBox() {
     setTweetImage("");
   };
 
+  const handleOpenEmoji = () => {
+    setShowEmojiPicker(!showEmojiPicker);
+  };
+
+  const onEmojiClick = (emojiObject) => {
+    setTweetMessage(tweetMessage + emojiObject.emoji);
+  };
+
+  const handleOpenImage = () => {
+    const fileInput = document.getElementById("image-input");
+    fileInput.click();
+  };
+
+  const handleImageSelect = (event) => {
+    const file = event.target.files[0];
+    setSelectedImage(file);
+    setTweetMessage(tweetMessage + file);
+  };
+
+  const closePreview = () => {
+    setSelectedImage(null);
+  };
+
   return (
     <div className="tweetBox">
       <form>
         <div className="tweetBox_input">
-          <Avatar style={{ height: '140px', width: '140px' }} src="https://dep.com.vn/wp-content/uploads/2022/11/phong-cach-thoi-trang-cha-eun-woo-1.jpg" />
-          <input
-            onChange={(e) => setTweetMessage(e.target.value)}
-            value={tweetMessage}
-            placeholder="What's happening?"
-            type="text"
+          <Avatar
+            style={{ height: "140px", width: "140px" }}
+            src="https://dep.com.vn/wp-content/uploads/2022/11/phong-cach-thoi-trang-cha-eun-woo-1.jpg"
           />
+          <TextField
+            autoFocus
+            margin="dense"
+            label="What's happening?"
+            type="text"
+            fullWidth
+            multiline
+            rows={6}
+            value={tweetMessage}
+            onChange={(e) => setTweetMessage(e.target.value)}
+            style={{
+              fontWeight: "bold",
+              color: "gray",
+              marginLeft: "30px",
+              marginTop: "5px",
+            }}
+            InputProps={{
+              endAdornment: (
+                <>
+                  <IconButton onClick={handleOpenEmoji}>
+                    <InsertEmoticon />
+                  </IconButton>
+                  <IconButton onClick={handleOpenImage}>
+                    <Image />
+                    <input
+                      id="image-input"
+                      type="file"
+                      accept="image/*"
+                      style={{ display: "none" }}
+                      onChange={handleImageSelect}
+                    />
+                  </IconButton>
+                </>
+              ),
+            }}
+          >
+            Enter your reply...
+          </TextField>
         </div>
-        <input
+        <TextField
           value={tweetImage}
           onChange={(e) => setTweetImage(e.target.value)}
           className="tweetBox_imageInput"
-          placeholder="Optional: Enter image URL"
+          label="Optional: Enter image URL"
           type="text"
+          InputProps={{ disableUnderline: !tweetImage }}
+          style={{
+            marginLeft: "30px",
+          }}
         />
-
+        {showEmojiPicker && (
+          <Picker onEmojiClick={onEmojiClick} height={400} width={300} />
+        )}
+        {selectedImage && (
+          <div>
+            <div className="flex flex-row ">
+              <div
+                style={{
+                  marginBottom: "10px",
+                  alignItems: "center",
+                }}
+              >
+                Image Preview:
+              </div>
+              <Button
+                variant="contained"
+                color="primary"
+                style={{
+                  border: "1px solid #C4C4C4",
+                  boxShadow: "2px solid rgba(0, 0, 0, 0.2)",
+                  width: "150px",
+                  marginLeft: "auto",
+                  textTransform: "none",
+                  borderRadius: "20px",
+                  marginBottom: "10px",
+                }}
+                onClick={closePreview}
+              >
+                Close Preview
+              </Button>
+            </div>
+            <img
+              src={URL.createObjectURL(selectedImage)}
+              alt="Selected image"
+              style={{
+                maxWidth: "50%",
+              }}
+            />
+          </div>
+        )}
         <Button
           onClick={sendTweet}
           type="submit"
