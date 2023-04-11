0a708fad4f317bbe35a1d4d8af7b6a82da468a51
Sam Lam <90791465+samlam67812@users.noreply.github.com>
2023-04-12 01:53:21 +0800

    adminTable could display successfully but need to be modified for crud

--- frontend/src/AdminTable.js
+++ frontend/src/AdminTable.js
@@ -1,26 +1,96 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import db from "./firebase";
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import { DataGrid, gridClasses } from '@mui/x-data-grid';
+import { grey } from '@mui/material/colors';
+
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import UsersActions from './UsersActions';
+
+
+
+// defining table 
+let rows = [
+];
+
+
 
 function AdminTable() {
 
     const [users, setUsers] = useState([]);
-    // fetch user info when rendering this component
+    const [open, setOpen] = useState(false);
+	const [addopen, setAddopen] = useState(false);
+    const [rowId, setRowId] = useState(0);
+	const [username, setUsername] = useState(null);
+    const [email, setEmail] = useState("");
+    const [pageSize, setPageSize] = useState(5);
 
+    const columns = useMemo(() => [
+        {
+            field: 'id',
+            headerName: 'ID',
+            headerClassName: 'header',
+            headerAlign: 'left',
+            align: 'left',
+            flex: 1,
+            minWidth: 300,
+            sortable: false
+        },
+        {
+            field: 'username',
+            headerName: 'Username',
+            headerClassName: 'header',
+            headerAlign: 'left',
+            align: 'left',
+            flex: 1,
+            minWidth: 300,
+            sortable: false
+        },
+        // {
+        //     field: 'email',
+        //     headerName: 'Email Address',
+        //     headerClassName: 'header',
+        //     headerAlign: 'left',
+        //     align: 'left',
+        //     flex: 1,
+        //     minWidth: 100,
+        // },
+        {
+            field: 'actions',
+            headerName: 'Actions',
+            type: 'actions',
+            renderCell: (params) => (
+              <UsersActions {...{ params, rowId, setRowId }} />
+            ),
+          },
+        ],
+        [rowId]
+    )
+
+
+    // fetch user info when rendering this component
     useEffect(() => {
         getUsers();
+        rows=users;
     }, [])
 
-    const getUsers = ( ) => {
-        db.collection("users").onSnapshot((snapShot) => {
-            console.log(snapShot.docs);
-            console.log("from admintable")
-            const userList = snapShot.docs.map((doc_user) => ({
-                id: doc_user.id,
-                ...doc_user.data(),
-            }));
+    const  getUsers = async () => {
+        const userList = [];
+        await db.collection("users").get()
+        .then((snapShot) => {
+            snapShot.forEach((doc) => {
+                let singleUser = {id: doc.id, ...doc.data()}
+                userList.push(singleUser)
+            });
             setUsers(userList);
         })
+        .catch((error) => console.log(error))
+        console.log("end getUsers")
     }
 
     const deleteUser = (id) => {
@@ -36,14 +106,27 @@ function AdminTable() {
   return (
 
     <div style={{ height: 500, width: '80%' }}>
-         <h3>admintable</h3>
-            {/* <DataGrid
-                rows={rows}
+            <DataGrid 
+                rows={users} 
                 columns={columns}
-                pageSize={10}
-                rowsPerPageOptions={[5]}
-                checkboxSelection
-            /> */}
+                getRowId={row=>row.id}
+
+                pageSize={pageSize}
+				onPageSizeChange={(newPageSize) => setPageSize(newPageSize)}
+				rowsPerPageOptions={[5, 10, 15]}
+                getRowSpacing={(params) => ({
+                    top: params.isFirstVisible ? 0 : 5,
+                    bottom: params.isLastVisible ? 0 : 5,
+                })}
+                sx={{
+                    [`& .${gridClasses.row}`]: {
+                      bgcolor: (theme) =>
+                        theme.palette.mode === 'light' ? grey[200] : grey[900],
+                    },
+                }}
+                onCellEditCommit={(params)=> setRowId(params.id)}
+				pagination
+                />            
     </div>
 
   )
