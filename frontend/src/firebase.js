d598a915d969e848b107a7e0f3261c1618b994c4
BillyNgEng <symmetrictensor@gmail.com>
2023-05-06 21:24:11 +0800

    firebase.js ChatGPT comment

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,3 +1,18 @@
+/*
+
+Documentation by ChatGPT (modified):
+
+The firebase.js file is used to connect to the Firebase Realtime Database and Cloud Firestore. 
+It imports the firebase module and initializes it with the firebaseConfig object, which contains the API key, authentication domain, project ID, storage bucket, messaging sender ID, app ID, and measurement ID.
+
+The file also initializes the Cloud Firestore service and exports it as a default module. 
+This allows other files in the project to import the database instance and use it to read and write data to the database.
+
+To use this file in your project, you will need to create a Firebase project and obtain the necessary credentials. 
+You can then copy and paste the firebaseConfig object into your own firebase.js file and import the database instance into your other project files.
+*/
+
+
 //import firebase from "firebase";
 import firebase from "firebase/app";
 import "firebase/firestore";
