6415bfc94358412af898d6c30c7f0ab35d909d2b
BillyNgEng <symmetrictensor@gmail.com>
2023-05-06 22:11:25 +0800

    SidebarOption.js ChatGPT comment

--- frontend/src/sidebar/SidebarOption.js
+++ frontend/src/sidebar/SidebarOption.js
@@ -1,3 +1,27 @@
+/*
+The program have referenced Rafeh Qazi's https://github.com/CleverProgrammers/twitter-clone with modification
+
+Documentation by ChatGPT (modified):
+
+The SidebarOption.js file is a React component that renders a sidebar option with an icon and text. 
+It imports the React library and the SidebarOption.css file for styling.
+
+The component takes in several props:
+
+active: a boolean value that determines whether the option is currently active or not
+text: a string that represents the text to be displayed for the option
+Icon: a component that represents the icon to be displayed for the option
+onClick: a function that is called when the option is clicked
+iconStyle: an optional object that represents the style to be applied to the icon
+
+The component returns a div element with the class sidebarOption. 
+If the active prop is true, the sidebarOption--active class is also applied. 
+When the option is clicked, the onClick function is called. 
+The Icon component is rendered with the optional iconStyle prop, and the text prop is rendered as an h2 element.
+
+Finally, the component is exported as the default export of the file, allowing it to be imported and used in other components.
+*/
+
 import React from "react";
 import "../css/SidebarOption.css";
 
