d941e0cd9065a1b7576fb72d625ae1c3bf9bf50f
BillyNgEng <symmetrictensor@gmail.com>
2023-05-06 17:44:58 +0800

    Comment.js ChatGPT comment

--- frontend/src/Comment.js
+++ frontend/src/Comment.js
@@ -1,3 +1,23 @@
+/*
+The program have referenced Rafeh Qazi's https://github.com/CleverProgrammers/twitter-clone with modification
+
+Documentation by ChatGPT (modified):
+
+Comment.js defines a React functional component named "Comment" that renders a comment box. It takes in the following props:
+
+avatar: A string representing the URL to the user's avatar image.
+displayName: A string representing the user's display name.
+image: A string representing the URL to an image associated with the comment (optional).
+text: A string representing the comment text.
+username: A string representing the user's username.
+verified: A boolean indicating whether the user is verified.
+
+The component renders these props along with an Avatar component from the Material-UI library and a VerifiedIcon component from the MUI icon library. 
+The comment box is structured using HTML div elements with CSS classes that style the layout. 
+The component is exported as the default export so it can be used in other modules.
+*/ 
+
+
 import React from "react";
 import "./css/Comment.css";
 import { Avatar } from "@material-ui/core";
