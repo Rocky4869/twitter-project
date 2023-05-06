3ecf0f4a76de5a153f54a0c716164c0c1fed32f6
BillyNgEng <symmetrictensor@gmail.com>
2023-05-06 19:08:41 +0800

    firebase.js ChatGPT comment

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,10 +1,24 @@
-//import firebase from "firebase";
+/*
+The program have referenced Rafeh Qazi's https://github.com/CleverProgrammers/twitter-clone with great modification
+
+Documentation by ChatGPT (modified):
+
+The firebase.js file contains the necessary code to connect to Firebase and initialize the Cloud Firestore service.
+
+First, the firebase and firestore modules are imported. 
+Then, the firebaseConfig object is defined, which contains the configuration settings for the Firebase project, including the API key, authentication domain, project ID, storage bucket, messaging sender ID, app ID, and measurement ID (which is optional for Firebase SDK v7.20.0 and later).
+
+Next, the firebase.initializeApp() method is called with the firebaseConfig object as its argument to initialize the Firebase app.
+
+Finally, the db constant is initialized with a reference to the Cloud Firestore service using the firebase.firestore() method, and is exported as the default export of the module.
+
+This file can be imported into other modules to access the Cloud Firestore service and perform CRUD operations on the database.
+
+*/
+
+
 import firebase from "firebase/app";
 import "firebase/firestore";
-
-//import firebase from 'firebase/app';
-//import 'firebase/compat/auth';
-//import 'firebase/firestore';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
@@ -22,31 +36,5 @@ firebase.initializeApp(firebaseConfig);
 
 // Initialize Cloud Firestore and get a reference to the service
 const db = firebase.firestore();
-
-//const firebaseApp = firebase.initializeApp(firebaseConfig);
-
-//const db = firebaseApp.firestore();
-
 export default db;
 
-//export const app = initializeApp(firebaseConfig);
-
-//const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
-//const db = getFirestore();
-//const storage = getStorage();
-
-//export default app;
-//export { db, storage };
-
-{
-  /*
-// Use this to initialize the firebase App
-const firebaseApp = firebase.initializeApp(firebaseConfig);
-
-// Use these for db & auth
-const db = firebaseApp.firestore();
-const auth = firebase.auth();
-
-export default { auth, db };
-*/
-}
